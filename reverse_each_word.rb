553fd327f50e4d63cbfca81114ffcc0f008e8750
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 12:18:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,17 +3,8 @@
 def reverse_each_word(words)
   words = "Hello there, and how are you?".split(" ")
     words.each do |word|
-      return word.reverse!
+      word.reverse!
     end
     sentence = words.reverse!
     sentence.join()
 end
-
-# def reverse_each_word(whatever)
-#   whatever = "Hello there, and how are you?".split(" ")
-#   whatever.collect do |whatever|
-#     puts whatever.reverse!
-#   end
-#   sentence = whatever.reverse!
-#   sentence.join(" ")
-# end
