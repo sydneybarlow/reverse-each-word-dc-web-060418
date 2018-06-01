9fd6ea2254483af6ff2b9b9ba16b6d3366a7f6d7
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 12:21:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 #invisible_array = ["try", "to", "code"]
 
 def reverse_each_word(words)
-  words = "Hello there, and how are you?".split(" ")
+  words = word.split(" ")
     words.each do |word|
       word.reverse!
     end
